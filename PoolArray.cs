df6dd610541eaff279f493fb7a7ae49153c76565
Mansur <56151168+IndieDeveloper-Mans@users.noreply.github.com>
2019-11-06 19:32:33 +0300

    Add files via upload

---
+++ PoolArray.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PoolArray : MonoBehaviour
+{
+    public Letter letter;
+    public GameObject grid;
+    public GameObject latter;
+
+    public char[] Array1 = new char[] { 'о', 'е', 'а', 'и', 'н', 'с', 'т', 'с', 'р', 'в' };
+    public char[] Array2 = new char[] { 'л', 'к', 'м', 'д', 'п', 'у', 'я', 'ы' };
+    public char[] Array3 = new char[] { 'ь', 'г', 'з', 'б', 'ч', 'й', 'х', 'ж' };
+    public char[] Array4 = new char[] { 'ш', 'ю', 'ц', 'щ', 'э', 'ф', 'ъ', 'ё' };
+
+
+    private void Start()
+    {
+        int randomizeArray = Random.Range(0, 5);
+ 
+        if (randomizeArray == 1)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                var latterPref = Instantiate(latter, Vector3.zero, Quaternion.identity, grid.transform);
+
+                latterPref.GetComponentInChildren<TextMeshProUGUI>().text = Array2[Random.Range(0, Array2.Length)].ToString();
+
+            }
+        }
+            
+        for (int i = 0; i < 40; i++)
+        {
+            var latterPref = Instantiate(latter, Vector3.zero, Quaternion.identity, grid.transform);
+
+            latterPref.GetComponentInChildren<TextMeshProUGUI>().text = Array1[Random.Range(0, Array1.Length)].ToString();
+
+        }
+
+        for (int i = 0; i < 30; i++)
+        {
+            var latterPref = Instantiate(latter, Vector3.zero, Quaternion.identity, grid.transform);
+
+            latterPref.GetComponentInChildren<TextMeshProUGUI>().text = Array2[Random.Range(0, Array2.Length)].ToString();
+
+        }
+
+        for (int i = 0; i < 20; i++)
+        {
+            var latterPref = Instantiate(latter, Vector3.zero, Quaternion.identity, grid.transform);
+
+            latterPref.GetComponentInChildren<TextMeshProUGUI>().text = Array3[Random.Range(0, Array3.Length)].ToString();
+
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            var latterPref = Instantiate(latter, Vector3.zero, Quaternion.identity, grid.transform);
+
+            latterPref.GetComponentInChildren<TextMeshProUGUI>().text = Array4[Random.Range(0, Array4.Length)].ToString();
+
+        }
+
+    }
+
+    public void FirstPool()
+    {
+        Random.Range(0, Array1.Length);
+    }
+}
---
+++ TimeManage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TimeManage : MonoBehaviour
+{
+    public float timer = 30f;
+    public TextMeshProUGUI time;
+    public Slider timeSlider;
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        int intTime = Mathf.RoundToInt(timer);
+
+        time.text = intTime.ToString();
+
+        timeSlider.value = intTime;
+
+        if (timer < 0 )
+        {
+            timer = 0f;
+            TimeLeft();
+        }
+    }
+
+    private void TimeLeft()
+    {
+
+    }
+}
